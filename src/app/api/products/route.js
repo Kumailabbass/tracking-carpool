dc3bdbe9e9e609e7654fc869c4eef3ccc67d4356
Kumail Abbas <kumailabbass2004@gmail.com>
2025-02-25 21:27:49 +0500

    first commit

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,15 +1,15 @@
 // app/api/products/route.js
 
-export async function GET(request) {
-    const dummyData = [
-      { id: 1, name: "Product 1", price: 100 },
-      { id: 2, name: "Product 2", price: 200 },
-      { id: 3, name: "Product 3", price: 300 }
-    ];
+// export async function GET(request) {
+//     const dummyData = [
+//       { id: 1, name: "Product 1", price: 100 },
+//       { id: 2, name: "Product 2", price: 200 },
+//       { id: 3, name: "Product 3", price: 300 }
+//     ];
   
-    return new Response(JSON.stringify(dummyData), {
-      status: 200,
-      headers: { "Content-Type": "application/json" }
-    });
-  }
+//     return new Response(JSON.stringify(dummyData), {
+//       status: 200,
+//       headers: { "Content-Type": "application/json" }
+//     });
+//   }
   
