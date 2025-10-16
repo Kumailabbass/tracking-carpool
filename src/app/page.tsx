60b6d49e05e2c4a059d9d2bcef4823c0c48b3d1e
Syed Kumail Abbas <126276119+Kumailabbass@users.noreply.github.com>
2025-10-16 15:51:42 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+
 
 const Login = () => {
   const router = useRouter();
